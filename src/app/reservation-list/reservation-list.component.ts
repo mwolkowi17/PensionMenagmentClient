d37b555c7a887aeaa74bca8b652f2004aa8ab71b
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2020-09-15 15:10:36 +0200

    Searching added to reservation module

--- src/app/reservation-list/reservation-list.component.ts
+++ src/app/reservation-list/reservation-list.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, ViewChild  } from '@angular/core';
-import {HttpService} from '../http.service';
-import {Reservation} from '../models/reservation.model';
-import {Guest} from '../models/guest.model';
-import {Room} from '../models/room.model';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpService } from '../http.service';
+import { Reservation } from '../models/reservation.model';
+import { Guest } from '../models/guest.model';
+import { Room } from '../models/room.model';
 import { NgForm } from '@angular/forms';
 import { Params } from '@angular/router';
 
@@ -14,7 +14,7 @@ import { Params } from '@angular/router';
 export class ReservationListComponent implements OnInit {
 
   public reservations: Reservation[];
-  public guests:Guest[];
+  public guests: Guest[];
   public rooms: Room[];
   public restype: number;
   public residguest: number;
@@ -22,12 +22,14 @@ export class ReservationListComponent implements OnInit {
   public rescheckout: string;
   public resbreakfestincluded: boolean;
   public newreservation: Reservation;
-  
+  public searchedguests: Guest[];
+  public searchedView: boolean;
+
 
 
   constructor(private httpService: HttpService) {
-    
-   }
+    this.searchedView = false;
+  }
 
   public getReservations = () => {
     let route: string = 'https://localhost:44343/api/Reservations';
@@ -38,7 +40,7 @@ export class ReservationListComponent implements OnInit {
         (error) => {
           console.error(error);
         });
-       
+
   }
 
   public getGuests = () => {
@@ -49,7 +51,7 @@ export class ReservationListComponent implements OnInit {
       },
         (error) => {
           console.error(error);
-        });  
+        });
   }
   public getRooms = () => {
     let route: string = 'https://localhost:44343/api/Rooms';
@@ -61,16 +63,16 @@ export class ReservationListComponent implements OnInit {
           console.error(error);
         });
   }
-  
-  public addReservation=()=>{
+
+  public addReservation = () => {
     //let route: string = 'https://localhost:44343/api/Reservations/addreservation?type=1&idguest=2&checkin=2020-09-13&checkout=2020-09-14&breakfestincluded=true';
-    let route: string = 'https://localhost:44343/api/Reservations/addreservation?type='+this.restype+'&idguest='+this.residguest+'&checkin='+this.rescheckin+'&checkout='+this.rescheckout+'&breakfestincluded='+this.resbreakfestincluded;
-    let params: Params ={
-      type:this.restype,
-      idguest:this.restype,
-      checkin:this.rescheckin,
-      checkout:this.rescheckout,
-      breakfestincluded:this.resbreakfestincluded
+    let route: string = 'https://localhost:44343/api/Reservations/addreservation?type=' + this.restype + '&idguest=' + this.residguest + '&checkin=' + this.rescheckin + '&checkout=' + this.rescheckout + '&breakfestincluded=' + this.resbreakfestincluded;
+    let params: Params = {
+      type: this.restype,
+      idguest: this.restype,
+      checkin: this.rescheckin,
+      checkout: this.rescheckout,
+      breakfestincluded: this.resbreakfestincluded
 
     }
     this.httpService.getData(route)
@@ -81,23 +83,37 @@ export class ReservationListComponent implements OnInit {
         (error) => {
           console.error(error);
         });
-       
+
   }
 
-  public onClear(id: number ){
-    
-    let route: string = 'https://localhost:44343/api/Reservations/'+id.toString();
-   
+  public onClear(id: number) {
+
+    let route: string = 'https://localhost:44343/api/Reservations/' + id.toString();
+
     this.httpService.removeGuest(route)
-    .subscribe((result) => {
-      // alert("udało się all")
-      this.getReservations();
-     },
-       (error) => {
-         console.error(error);
-       });
+      .subscribe((result) => {
+        // alert("udało się all")
+        this.getReservations();
+      },
+        (error) => {
+          console.error(error);
+        });
     this.getGuests();
   }
+
+  public getSearchedGuests = (surname: string) => {
+
+    let route: string = 'https://localhost:44343/api/Guests/FindGuestNumber?guestsurname=' + surname;
+    this.httpService.getData(route)
+      .subscribe((result) => {
+        this.searchedguests = result as Guest[];
+      },
+        (error) => {
+          console.error(error);
+        });
+
+  }
+
   @ViewChild('f', { static: false }) carForm: NgForm;
 
   onSubmit(form: NgForm) {
@@ -105,21 +121,29 @@ export class ReservationListComponent implements OnInit {
     console.log(form.value.type);
     console.log(form.value.checkin);
     console.log(form.value.checkout);
-    this.restype=form.value.type;
-    this.residguest=form.value.idguest;
-    this.rescheckin=form.value.checkin;
-    this.rescheckout=form.value.checkout;
-    this.resbreakfestincluded=form.value.breakfestincluded;
-    console.log("guest:"+this.residguest)
+    this.restype = form.value.type;
+    this.residguest = form.value.idguest;
+    this.rescheckin = form.value.checkin;
+    this.rescheckout = form.value.checkout;
+    this.resbreakfestincluded = form.value.breakfestincluded;
+    console.log("guest:" + this.residguest)
     this.addReservation();
-    
+
   }
 
+  @ViewChild('g', { static: false }) surnameForm: NgForm;
+
+  onSubmitSearch(form: NgForm) {
+    console.log(form.value.guestname)
+    this.getSearchedGuests(form.value.guestname);
+    this.searchedView = true;
+
+  }
   ngOnInit(): void {
     this.getReservations();
     this.getGuests();
     this.getRooms();
-    
+
   }
 
 }
