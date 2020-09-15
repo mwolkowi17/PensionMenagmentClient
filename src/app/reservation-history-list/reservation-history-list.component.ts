ef84de3e67647c262a092a34c0ad50c2504d498b
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2020-09-15 21:30:42 +0200

    begin_of_reservation_history_module

--- src/app/reservation-history-list/reservation-history-list.component.ts
+++ src/app/reservation-history-list/reservation-history-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Reservation } from '../models/reservation.model';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-reservation-history-list',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReservationHistoryListComponent implements OnInit {
 
-  constructor() { }
+  public reservationHistoryItems: Reservation[];
+  //need a new model - not Reservation type!!!
 
+  constructor(private httpService: HttpService) { 
+
+  }
+
+  public getHistoryReservations = () => {
+    let route: string = 'https://localhost:44343/api/ReservationHistories';
+    this.httpService.getData(route)
+      .subscribe((result) => {
+        this.reservationHistoryItems = result as Reservation[];
+        
+      },
+        (error) => {
+          console.error(error);
+        });
+
+  }
   ngOnInit(): void {
+
+    this.getHistoryReservations()
+    
   }
 
 }
