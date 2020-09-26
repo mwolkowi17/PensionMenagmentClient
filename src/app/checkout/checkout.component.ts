20a80f110052fe834f06515632cae5e2edd36e20
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2020-09-26 19:00:56 +0200

    checkout_component_display

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Room } from '../models/room.model';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-checkout',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
+  public roomstodisplay:Room[];
 
-  constructor() { }
+  constructor(private httpService: HttpService) {
+
+   }
+
+   public getRoomstodisplay = () => {
+    let route: string = 'https://localhost:44343/api/Checkout';
+    this.httpService.getData(route)
+      .subscribe((result) => {
+        this.roomstodisplay = result as Room[];
+        
+      },
+        (error) => {
+          console.error(error);
+        });
+    
+    
+
+  }
+
 
   ngOnInit(): void {
+    this.getRoomstodisplay();
   }
 
 }
