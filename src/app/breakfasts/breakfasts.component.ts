3ac1c7a3c1ed55f03e8f2e44175fd5675aef4374
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2020-09-28 08:24:54 +0200

    breakfest tab begin of work

--- src/app/breakfasts/breakfasts.component.ts
+++ src/app/breakfasts/breakfasts.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Reservation } from '../models/reservation.model';
+import { Guest } from '../models/guest.model';
+import { Room } from '../models/room.model';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-breakfasts',
@@ -7,9 +11,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreakfastsComponent implements OnInit {
 
-  constructor() { }
+  public reservations: Reservation[];
+  public guests: Guest[];
+  public rooms: Room[];
+  public breakfastlist : Reservation[];
+
+  constructor(private httpService: HttpService) {
+
+   }
+
+  public getReservations = () => {
+    let route: string = 'https://localhost:44343/api/Reservations';
+    this.httpService.getData(route)
+      .subscribe((result) => {
+        this.reservations = result as Reservation[];
+      },
+        (error) => {
+          console.error(error);
+        });
+
+  }
+
+  public getGuests = () => {
+    let route: string = 'https://localhost:44343/api/Guests';
+    this.httpService.getData(route)
+      .subscribe((result) => {
+        this.guests = result as Guest[];
+      },
+        (error) => {
+          console.error(error);
+        });
+  }
+  public getRooms = () => {
+    let route: string = 'https://localhost:44343/api/Rooms';
+    this.httpService.getData(route)
+      .subscribe((result) => {
+        this.rooms = result as Room[];
+      },
+        (error) => {
+          console.error(error);
+        });
+  }
+
+  public getBreakfasts=()=>{
+
+    
+  }
 
   ngOnInit(): void {
+    this.getReservations();
+    this.getGuests();
+    this.getRooms();
   }
 
 }
